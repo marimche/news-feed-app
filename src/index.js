8cf4f42df88748afd9134ca75244f0ea7789b614
March <mch@mail.com>
2021-03-03 19:23:06 +0300

    0.5

--- src/index.js
+++ src/index.js
@@ -4,15 +4,19 @@ import {BrowserRouter as Router} from 'react-router-dom'
 
 import Routes from 'routes'
 import TopBar from 'components/topBar'
+import {CurrentUserProvider} from 'contexts/currentUser'
+import CurrentUserChecker from 'components/currentUserChecker'
 
 const App = () => {
   return (
-    <div>
-      <Router>
-        <TopBar />
-        <Routes />
-      </Router>
-    </div>
+    <CurrentUserProvider>
+      <CurrentUserChecker>
+        <Router>
+          <TopBar />
+          <Routes />
+        </Router>
+      </CurrentUserChecker>
+    </CurrentUserProvider>
   )
 }
 
